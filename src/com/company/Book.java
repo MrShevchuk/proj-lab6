2bd42041c740f57192ed5f89b05206ac27dbef24
MrShevchuk <viacheslavshevchuk911@gmail.com>
2020-04-20 12:34:34 +0300

    fix errors. Need one more step to finish

--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -1,6 +1,5 @@
 package com.company;
-
-import java.util.Arrays;
+import static java.util.Objects.nonNull;
 
 public class Book {
 
@@ -8,13 +7,80 @@ public class Book {
     public String Author;
     public String PublishedYear;
     public int SheetNumber;
+    public String[] arr;
+    public int[] chapters;
 
-        public String[] pageTurning ( int count){
-            String[] page = new String[count];
-            for (int i = 0; i < count; i++) {
-                page[i] = (i + 1) + " стр.";
-            }
-            return page;
-        }
+    public String[] getArr() {
+        return arr;
     }
+
+    public void setArr(String[] arr) {
+        this.arr = arr;
+    }
+
+    public int[] getChapters() {
+        return chapters;
+    }
+
+    public void setChapters(int[] chapters) {
+        this.chapters = chapters;
+    }
+
+    public String getBookName() {
+        return BookName;
+    }
+
+    public void setBookName(String bookName) {
+        BookName = bookName;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public String getPublishedYear() {
+        return PublishedYear;
+    }
+
+    public void setPublishedYear(String publishedYear) {
+        PublishedYear = publishedYear;
+    }
+
+    public int getSheetNumber() {
+        return SheetNumber;
+    }
+
+    public void setSheetNumber(int sheetNumber) {
+        SheetNumber = sheetNumber;
+    }
+
+    public String[] pageTurning (int count) {
+        String[] pages = new String[count];
+        for (int i = 0; i < count; i++) {
+            pages[i] = (i + 1) + " стр.";
+        }
+        return pages;
+    }
+
+    public static String[][] generateChaptersBySheets (String[] sheets, int chaptersCount) {
+//        if(nonNull(sheets)) {
+//            throw new RuntimeException("глав не должно быть меньше чем 1");
+//        }
+//        if(chaptersCount <= 0) {
+//            throw new RuntimeException("глав не должно быть меньше чем 1");
+//        }
+        int size = sheets.length/chaptersCount;
+        String[][] chapters = new String[chaptersCount][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = i; j < size; j++) {
+                chapters[i][j] = sheets [j+size*i];
+            }
+        }
+        return chapters;
+    }
+}
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,21 @@
 package com.company;
 
 import java.util.Arrays;
+import static com.company.Book.generateChaptersBySheets;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Book TheGreatGatsby = new Book();
+        TheGreatGatsby.setArr(TheGreatGatsby.pageTurning(174));
+        System.out.println(Arrays.toString(TheGreatGatsby.getChapters()));
+        String[][] result = generateChaptersBySheets(TheGreatGatsby.getArr(), 9);
+        System.out.println("Книга разбита на главы");
 
         TheGreatGatsby.BookName = "The Great Gatsby";
         TheGreatGatsby.Author = "F.Scott Fitzgerald";
         TheGreatGatsby.PublishedYear = "1925";
         TheGreatGatsby.SheetNumber = 174;
-
-        String[] arr = TheGreatGatsby.pageTurning(174);
-        System.out.println(Arrays.toString(arr));
     }
 }
